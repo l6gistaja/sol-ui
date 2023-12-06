e995fcc862d0a6f22bd97ab07b1d5b7edf762def
Juks <juks@alkohol.ee>
2023-12-06 21:31:44 +0200

    login page refactored

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,34 +1,22 @@
 <template>
-  <br/><br/>
-  <authenticator>
-    <template v-slot="{ user, signOut }">
-      <PageTitle>Welcome to Soleron user interface!</PageTitle>
-    </template>
-  </authenticator>
+  <div class="aws-auth-wrapper">
+    <authenticator>
+      <template v-slot="{ user, signOut }">
+        <PageTitle>Welcome to Soleron user interface!</PageTitle>
+      </template>
+    </authenticator>
+  </div>
 </template>
 
 <script setup>
-
   import "@aws-amplify/ui-vue/styles.css";
   import { Amplify } from 'aws-amplify';
   import awsconfig from '../aws-exports';
   Amplify.configure(awsconfig);
-
   import PageTitle from '../components/PageTitle.vue'
-
 </script>
 
-<script>
-
-export default {
-  data() {
-    return {
-
-    };
-  },
-  methods: {
-
-  },
-};
-</script>
+<style scoped>
+  .aws-auth-wrapper { padding-top: 1em; }
+</style>
 
